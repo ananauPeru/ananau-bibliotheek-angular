1f888123fad6101510f4d7b272d704bc7d9794a1
Martijn Bogaert <martijn.bogaert@hotmail.com>
2021-02-23 16:30:35 +0100

    Change environment api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://ananauapi.azurewebsites.net/api',
-  //apiUrl: 'https://localhost:5001/api',
+  //apiUrl: 'https://ananauapi.azurewebsites.net/api',
+  apiUrl: 'https://localhost:5001/api',
   production: true
 };
