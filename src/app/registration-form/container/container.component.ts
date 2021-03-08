ec8cc6a8f1ab2634a16d655eb9692c49b4e17b28
Martijn Bogaert <martijn.bogaert@student.hogent.be>
2021-03-08 19:37:48 +0100

    Add functions for calculating the progress of the progress bar

--- src/app/registration-form/container/container.component.ts
+++ src/app/registration-form/container/container.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 
 @Component({
   selector: "app-container",
@@ -72,6 +79,91 @@ export class ContainerComponent implements OnInit {
   }
 
   onSumbit() {
-    console.log(this.formContainer);
+    console.log(ContainerComponent.countFields(this.formContainer));
+  }
+
+  /**
+   * Returns for a given AbstractControl the amount of FormControls, the amount of FormControls labelled as required and the amount of FormControls labelled as required and valid.
+   *
+   * @remarks
+   * This method is loosely based on the discussion found {@link https://stackoverflow.com/questions/49778432/reactive-forms-count-all-formcontrols-in-a-formgroup here}.
+   *
+   * @param control - The `AbstractControl` to be processed
+   *
+   * @returns An object containing three numbers:
+   * 1) `all`: The amount of all objects of type `FormControl` present in the given `AbstractControl`
+   * 2) `required`: The amount of all objects of type `FormControl`, containing a `required` validator, present in the given `AbstractControl`
+   * 3) `requiredAndValid`: The amount of all objects of type `FormControl`, containing a `required` validator and labelled as `valid`, present in the given `AbstractControl`
+   */
+  static countFields(
+    control: AbstractControl
+  ): { all: number; required: number; requiredAndValid: number } {
+    if (control instanceof FormControl) {
+      let isRequired = ContainerComponent.isFieldRequired(control);
+      return {
+        all: 1,
+        required: isRequired ? 1 : 0,
+        requiredAndValid: isRequired && control.valid ? 1 : 0,
+      };
+    }
+
+    if (control instanceof FormArray) {
+      return control.controls.reduce(
+        (valuesObject, currentControl) => {
+          const intermediateResult = ContainerComponent.countFields(
+            currentControl
+          );
+          return {
+            all: valuesObject.all + intermediateResult.all,
+            required: valuesObject.required + intermediateResult.required,
+            requiredAndValid:
+              valuesObject.requiredAndValid +
+              intermediateResult.requiredAndValid,
+          };
+        },
+        { all: 0, required: 0, requiredAndValid: 0 }
+      );
+    }
+
+    if (control instanceof FormGroup) {
+      return Object.keys(control.controls)
+        .map((key) => control.controls[key])
+        .reduce(
+          (valuesObject, currentControl) => {
+            const intermediateResult = ContainerComponent.countFields(
+              currentControl
+            );
+            return {
+              all: valuesObject.all + intermediateResult.all,
+              required: valuesObject.required + intermediateResult.required,
+              requiredAndValid:
+                valuesObject.requiredAndValid +
+                intermediateResult.requiredAndValid,
+            };
+          },
+          { all: 0, required: 0, requiredAndValid: 0 }
+        );
+    }
+  }
+
+  /**
+   * Returns a boolean indicating whether or not the given FormControl is labelled as required.
+   *
+   * @remarks
+   * This method is loosely based on the discussion found {@link https://stackoverflow.com/questions/39819123/angular2-find-out-if-formcontrol-has-required-validator here}.
+   *
+   * @param control - The `FormControl` to be processed
+   *
+   * @returns A boolean where `true` indicates a `required` validator is present and `false` indicates no `required` validator is present
+   */
+  static isFieldRequired(control: FormControl): boolean {
+    let isRequired = false;
+    if (control.validator) {
+      const validator = control.validator({} as FormControl);
+      if (validator && validator.required) {
+        isRequired = true;
+      }
+    }
+    return isRequired;
   }
 }
