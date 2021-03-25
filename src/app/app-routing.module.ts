36e4bec180e47a1e01a648ee0fe18a70adc8db1c
Martijn Bogaert <martijn.bogaert@student.hogent.be>
2021-03-25 14:46:32 +0100

    Shield registration module from anonymous users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,7 @@ const routes: Routes = [
   },
   {
     path: "registration",
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import("./registration-form/registration-form.module").then(
         (mod) => mod.RegistrationFormModule
