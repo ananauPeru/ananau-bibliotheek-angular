2030161f75d8753be8114d44e173fc25d5a3e429
Martijn Bogaert <martijn.bogaert@student.hogent.be>
2021-05-07 17:41:31 +0200

    Design dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthUtil } from "src/app/_utils/auth_util";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
+  constructor(public AuthUtil: AuthUtil) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard.component";
+import { TranslationModule } from "src/app/modules/i18n/translation.module";
 
 @NgModule({
   declarations: [DashboardComponent],
@@ -13,6 +14,7 @@ import { DashboardComponent } from "./dashboard.component";
         component: DashboardComponent,
       },
     ]),
+    TranslationModule,
   ],
 })
 export class DashboardModule {}
