5f181dc03a65e5cfff69f1a9c34d70ba4a6bb225
Lars Inslegers <lars.inslegers@student.hogent.be>
2021-10-25 16:02:08 +0200

    start aanmaken model van lessen

---
+++ src/app/modules/library/_dto/class-dto.ts
@@ -0,0 +1,8 @@
+export class ClassDTO {
+    Title: string
+    Autor: string
+    Description: string
+    Public: string
+    PdfUrl: string
+    CreationDate: Date
+  }
---
+++ src/app/modules/library/_models/class.model.ts
@@ -0,0 +1,13 @@
+export class ClassModel {
+  classID: number
+  Title: string
+  Autor: string
+  Description: string
+  Public: string
+  PdfUrl: string
+  CreationDate: Date
+
+  constructor() {}
+
+ 
+}
