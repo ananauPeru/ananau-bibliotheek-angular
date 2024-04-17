589b825a7c907db0227e640221b1a089c021bc91
Natalie <nataliia.chapaieva@student.hogent.be>
2024-04-17 13:01:52 -0500

    registrations/volonteers

--- src/app/modules/registration-form/data-services/registration.service.ts
+++ src/app/modules/registration-form/data-services/registration.service.ts
@@ -43,7 +43,7 @@ export class RegistrationService {
         map(
           (dto: any): RegistrationDTO => {
             console.log(dto);
-            return dto;
+            return dto.details;
           }
         )
       );
