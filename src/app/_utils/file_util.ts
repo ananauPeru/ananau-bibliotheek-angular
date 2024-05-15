fd1d4a9e32c627f97896e2b90cd204f1d4290473
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-05-15 11:44:44 -0500

    Added methods to universalise getting audio/image files

--- src/app/_utils/file_util.ts
+++ src/app/_utils/file_util.ts
@@ -2,10 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable, from, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
+const IMAGE_FILE_EXTENSIONS: string[] = ['jpg', 'png', 'jpeg'];
+const AUDIO_FILE_EXTENSIONS: string[] = ['mp3', 'wav'];
+
 @Injectable({
   providedIn: 'root'
 })
 export class FileUtil {
+
+
+  /**
+   * Converts a file url to a File object
+   * @param url the url of the file to be converted to a File object
+   * @returns an observable of the File object
+   */
   public urlToFile(url: string): Observable<File> {
     const fileName = url.split("/").pop();
     return from(fetch(url)).pipe(
@@ -19,5 +29,27 @@ export class FileUtil {
         return of(file);
       })
     );
+  }
+
+  /**
+   * Returns true if the file is an image file, false otherwise
+   * @param fileUrl the url of the file to be checked
+   * @returns true if the file is an image file, false otherwise
+   */
+  public isImageFile(fileUrl: string): boolean {
+    return IMAGE_FILE_EXTENSIONS.includes(this.getFileExtension(fileUrl));
+  }
+
+  /**
+   * Returns true if the file is an audio file, false otherwise
+   * @param fileUrl the url of the file to be checked
+   * @returns true if the file is an audio file, false otherwise
+   */
+  public isAudioFile(fileUrl: string): boolean {
+    return AUDIO_FILE_EXTENSIONS.includes(this.getFileExtension(fileUrl));
+  }
+
+  private getFileExtension(fileUrl: string): string {
+    return fileUrl.split('.').pop();
   }
 }
