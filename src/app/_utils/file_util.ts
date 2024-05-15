a288890af6f0e86aa2e0c6040b201a23224c7448
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-05-15 11:08:57 -0500

    Fixed a bug with the FileUtil that renamed files, not the way I wanted it

--- src/app/_utils/file_util.ts
+++ src/app/_utils/file_util.ts
@@ -7,12 +7,13 @@ import { switchMap } from 'rxjs/operators';
 })
 export class FileUtil {
   public urlToFile(url: string): Observable<File> {
+    const fileName = url.split("/").pop();
     return from(fetch(url)).pipe(
       switchMap(res => from(res.blob())),
       switchMap(blob => {
         const mime = blob.type;
         const ext = mime.slice(mime.lastIndexOf("/") + 1, mime.length);
-        const file = new File([blob], `filename.${ext}`, {
+        const file = new File([blob], fileName, {
           type: mime,
         });
         return of(file);
