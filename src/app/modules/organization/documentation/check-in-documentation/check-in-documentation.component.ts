1f32df1ffbe436edc533f42614264a289c94719e
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-05-21 11:51:42 -0500

    Added qr-code documentation

---
+++ src/app/modules/organization/documentation/check-in-documentation/check-in-documentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-in-documentation',
+  templateUrl: './check-in-documentation.component.html',
+  styleUrls: ['./check-in-documentation.component.scss']
+})
+export class CheckInDocumentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
